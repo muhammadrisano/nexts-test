5213325789f02fae933117d08ed84a2b5c10333d
ChanraSB <email_addresschanrasbakkara@gmail.com>
2024-02-01 01:39:03 +0700

    fourth commit

--- app/main/profile/edit/page.js
+++ app/main/profile/edit/page.js
@@ -9,7 +9,7 @@ const EditProfile = () => {
       <div className="container" style={{ paddingTop: "50px" }}>
         <div className="row row-cols-sm-1 row-cols-md-1 row-cols-lg-2 row-cols-xl-2 d-flex justify-content-center ">
           <div className="col-sm-12 col-md-12 col-lg-4 col-xl-4 d-flex justify-content-center">
-            <CardEditProfile child="Edit" href="/main/profile/edit" />
+            <CardEditProfile child="Edit" />
           </div>
           <div className="col-sm-12 col-md-12 col-lg-8 col-xl-8  pt-4 px-4" style={{ borderRadius: "10px", background: "white" }}>
             <EditForm />
--- component/module/Navbar/Navbar.js
+++ component/module/Navbar/Navbar.js
@@ -1,11 +1,23 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import logo from "@/public/images/navbarLogo.svg?url";
 import { FaRegBell } from "react-icons/fa6";
 import { MdMailOutline } from "react-icons/md";
 import { FaUserCircle } from "react-icons/fa";
 import Link from "next/link";
+import Button from "@/component/base/button/button";
+import { useRouter } from "next/navigation";
 const Navbar = () => {
+  const router = useRouter();
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setLoggedIn(true);
+    }
+  }, []);
   return (
     <nav className=" navbar navbar-expand-lg bg-light fixed-top  ">
       <div className="container-fluid ">
@@ -17,21 +29,46 @@ const Navbar = () => {
         </button>
         <div className="collapse navbar-collapse  justify-content-end" id="navbarSupportedContent">
           <ul className=" nav navbar-nav  me-5 mb-2 mb-lg-0 justify-content-end ">
-            <li className="nav-item me-4">
-              <Link className="nav-link" href="#">
-                <FaRegBell />
-              </Link>
-            </li>
-            <li className="nav-item me-4">
-              <Link className="nav-link" href="#">
-                <MdMailOutline />
-              </Link>
-            </li>
-            <li className="nav-item me-4">
-              <Link className="nav-link" href="/main/profile">
-                <FaUserCircle />
-              </Link>
-            </li>
+            {isLoggedIn ? (
+              <>
+                <li className="nav-item me-4">
+                  <Link className="nav-link" href="#">
+                    <FaRegBell />
+                  </Link>
+                </li>
+                <li className="nav-item me-4">
+                  <Link className="nav-link" href="#">
+                    <MdMailOutline />
+                  </Link>
+                </li>
+                <li className="nav-item me-4">
+                  <Link className="nav-link" href="/main/profile">
+                    <FaUserCircle />
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="mx-1">
+                  <Button
+                    child="masuk"
+                    style={{ color: "#5E50A1", backgroundColor: "white", width: "100%", border: "1px solid #5E50A1" }}
+                    onClick={() => {
+                      router.push(`/auth/login`);
+                    }}
+                  />
+                </li>
+                <li className="mx-1">
+                  <Button
+                    child="daftar"
+                    style={{ color: "white", backgroundColor: "#5E50A1", width: "100%" }}
+                    onClick={() => {
+                      router.push(`/auth/register`);
+                    }}
+                  />
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </div>
